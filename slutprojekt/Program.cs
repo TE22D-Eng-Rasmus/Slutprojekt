86b90051194721057c595dedbeadea423af5c48d
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2024-02-22 10:37:05 +0100

    början

---
+++ slutprojekt/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+using Raylib_cs;
+
+Raylib.InitWindow(1980,1080,"shuno");
+Raylib.ToggleFullscreen();
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose()){
+Raylib.BeginDrawing();
+
+Raylib.ClearBackground(Color.DarkGray);
+
+Raylib.EndDrawing();
+
+}
+
+class player{
+
+
+}
