499b16d6e1837730ac67275a50b43d07fbc3332d
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2024-03-19 10:54:12 +0100

    lagt till gravity

--- slutprojekt/Player.cs
+++ slutprojekt/Player.cs
@@ -1,5 +1,7 @@
 // See https://aka.ms/new-console-template for more information
+using System.Drawing;
 using System.Numerics;
+using System.Security;
 using Raylib_cs;
 
 class player
@@ -9,8 +11,9 @@ public int speed = 0;
 
 public Vector2 movement = new(0,0);
 
-public Vector2 position = new(Raylib.GetScreenWidth() /2, Raylib.GetScreenHeight() /2);
+public Vector2 position = new(Raylib.GetScreenWidth() /8, 1000 - 50);
 
 public Vector2 size = new(50,50);
 
+public float gravity = 0.35f;
 }
--- slutprojekt/Program.cs
+++ slutprojekt/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using Raylib_cs;
 
 
@@ -9,38 +10,52 @@ Raylib.ToggleFullscreen();
 Raylib.SetTargetFPS(60);
 
 player player = new player();
-Rectangle playerRect = new Rectangle(player.position,player.size);
+Rectangle playerRect = new Rectangle(player.position, player.size);
 
 while (!Raylib.WindowShouldClose())
 {
 
-playerRect.Position = player.position;
+ float gravity = 0.35f;
 
-player.movement.X = 0;
+    playerRect.Position = player.position;
 
-player.movement.Y = 0;
+    player.movement.X = 0;
 
-if (Raylib.IsKeyDown(KeyboardKey.W)){
-    player.movement.Y = -10;
-}
-if (Raylib.IsKeyDown(KeyboardKey.S)){
-    player.movement.Y = 10;
-}
-if (Raylib.IsKeyDown(KeyboardKey.D)){
-    player.movement.X = 10;
-}
-if (Raylib.IsKeyDown(KeyboardKey.A)){
-    player.movement.X = -10;
-}
+    
+
+    if (Raylib.IsKeyPressed(KeyboardKey.W))
+    {
+        player.movement.Y = -10;
+        // gravity += 1;
+
+    }
+    
+    
+    if (Raylib.IsKeyDown(KeyboardKey.D))
+    {
+        player.movement.X = 10;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.A))
+    {
+        player.movement.X = -10;
+    }
 
 
-Vector2.Normalize(player.movement);
-player.position +=player.movement;
+
+
+    
+
+
+   // Vector2.Normalize(player.movement);
+    player.position += player.movement;
+    
 
 
     Raylib.BeginDrawing();
 
     Raylib.ClearBackground(Color.DarkGray);
+
+    Raylib.DrawRectangle(0,1000,5000,500, Color.Green);
 
     Raylib.DrawRectangleRec(playerRect, Color.Magenta);
 
